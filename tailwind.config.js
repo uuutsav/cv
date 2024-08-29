775c87a4c45e27c5ada611c1bd9f611fe172c737
uuutsav <21051903@kiit.ac.in>
2024-08-30 05:23:43 +0530

    Add About Me section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './index.html',            // Match the index.html file in the root directory
-    './pages/**/*.{html}',     // Match all HTML files in the pages directory and its subdirectories
+    './pages/**/*.html',     // Match all HTML files in the pages directory and its subdirectories
   ],
   theme: {
     extend: {},
